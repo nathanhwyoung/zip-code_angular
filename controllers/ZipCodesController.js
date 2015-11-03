32f2380f8fa5d8526c9b97f706b73bac70cd9968
Nathan Young <nathanyoung@Nathans-MacBook-Pro.local>
2015-11-02 20:57:26 -0800

    add new firebase url

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -1,10 +1,19 @@
 zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
 
-    $scope.myData = new Firebase('https://kshj17qyrg1.firebaseio-demo.com');
+    $scope.myData = new Firebase('https://incandescent-heat-6705.firebaseio.com/');
     $scope.myData.on('value', function(snapshot) {
-        console.log("HELLO");
-        // console.log(snapshot.val());
-        // $scope.allZipCodes = snapshot.val();
+        // console.log("HELLO");
+
+        //returns null
+        console.log(snapshot.val());
+
+
+        $scope.allZipCodes = snapshot.val();
+
+        // allZipCodes is not defined
+        console.log(allZipCodes);
+
+
 
     });
 
