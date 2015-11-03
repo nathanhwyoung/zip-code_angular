3ce61cc3803761281dfb66c90c1f664b6c3a2be4
Nathan Young <nathanyoung@Nathans-MacBook-Pro.local>
2015-11-02 21:45:54 -0800

    edit firebase file

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -2,15 +2,8 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
 
     $scope.myData = new Firebase('https://incandescent-heat-6705.firebaseio.com/');
     $scope.myData.on('value', function(snapshot) {
-        // console.log("HELLO");
-
-        //returns null
         console.log(snapshot.val());
-
-
         $scope.allZipCodes = snapshot.val();
-
-        // allZipCodes is not defined
         console.log(allZipCodes);
 
 
