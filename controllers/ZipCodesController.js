26316d1cc0f7e7752c945404b0657865f60cc39c
Nathan Young <student@epicodus.com>
2015-11-11 12:00:50 -0800

    comments

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -1,13 +1,25 @@
 zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
 
+    // create firebase reference
     $scope.myData = new Firebase('https://incandescent-heat-6705.firebaseio.com/');
+
+    /**
+    call the 'on' method on the reference object. this method receives two
+    arguments: an event type(string), and a callback function that fires
+    when the specified event occurs. 'value' will trigger once with the
+    initial data stored at this location, and then trigger again each time the
+    data changes. the data snapshot passed to the callback will be for the
+    location as which on() was called. it won't trigger until the entire
+    contents has been synchronized. if the location has no data, it will be
+    triggered with an empty DataSnapshot.
+    **/
+
     $scope.myData.on('value', function(snapshot) {
-        console.log(snapshot.val());
+        // console.log(snapshot.val());
+        // console.log("test");
+
         $scope.allZipCodes = snapshot.val();
-        console.log(allZipCodes);
-
-
-
+        // console.log(allZipCodes.Zipcode);
     });
 
 });
