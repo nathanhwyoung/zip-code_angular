efc804e473abb3d8355abed20d77365fe62a34fd
Nathan <student@epicodus.com>
2015-11-16 15:17:54 -0800

    php

--- zips.php
+++ zips.php
@@ -1,13 +1,37 @@
 <?php
 
-    class
+    // POPULATIONS
+    $populations = file_get_contents("zipcodespopulation.json");
 
-    $zips = file_get_contents("zipcodestest.json");
+    $decoded_pops = json_decode($populations, true);
+
+    // ZIPS, CITIES, STATES
+    $zips = file_get_contents("zipcodes.json");
+
     $decoded_zips = json_decode($zips, true);
-    foreach ($decoded_zips as $key => $value) {
-        echo $value['Zipcode'] . " " . $value['City'] . " " . $value['State'] . " ";
+
+    // foreach ($decoded_zips as $key => $value) {
+    //     // echo $value['Zipcode'] . " " . $value['City'] . " " . $value['State'] . " \n";
+    //     $zipCode = $value['Zipcode'];
+    //     $city = $value['City'];
+    //     $state = $value['State'];
+    //     echo $zipCode . " " . $city . " " . $state . " \n";
+    // }
+
+    foreach ($decoded_pops as $key => $value) {
+        echo $value['Zip Code ZCTA'] . " " . $value['2010 Census Population'] . " \n";
     }
-    // var_dump($decoded_zips);
+
+
+
+    // foreach ($decoded_pops as $key => $value) {
+    //   if ($value['Zip Code ZCTA'] == $zipCode) {
+    //     $return_population = $value['2010 Census Population'];
+    //     return $return_population;
+    //   }
+    //   // return $return_population;
+    // }
+    // $population = $return_population;
 
 
 
