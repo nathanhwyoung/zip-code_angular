9c9b54b06d465473656e57f4d71f25dc50f973af
Nathan <student@epicodus.com>
2015-11-17 12:26:56 -0800

    add lat & long

--- zips.php
+++ zips.php
@@ -1,22 +1,26 @@
 <?php
 
     $populations = file_get_contents("zipcodespopulation.json");
-
     $decoded_pops = json_decode($populations, true);
 
     $zips = file_get_contents("zipcodes.json");
-
     $decoded_zips = json_decode($zips, true);
 
     foreach ($decoded_zips as $key => $value) {
         $zipCode = $value['Zipcode'];
         $city = $value['City'];
         $state = $value['State'];
+        $lat = $value['Lat'];
+        $long = $value['Long'];
 
-        foreach ($decoded_pops as $key => $value) {
-          if ($value['Zip Code ZCTA'] == $zipCode) {
-            echo "population of " . $zipCode . " " . $city . " " . $state . " " .
-                $value['Zip Code ZCTA'] ." " . $value['2010 Census Population'] . "\n";
+        foreach ($decoded_pops as $key => $pop_value) {
+          if ($pop_value['Zip Code ZCTA'] == $zipCode) {
+            echo $zipCode . " " .
+                 $city . " " .
+                 $state . " " .
+                 $lat . " " .
+                 $long . " " .
+                 $pop_value['2010 Census Population'] . "\n";
             break;
           }
         }
