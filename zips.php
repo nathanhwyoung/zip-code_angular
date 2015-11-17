8588fb6bc74df9deb79087cf10f8a8b4d2ad231a
Nathan <student@epicodus.com>
2015-11-16 16:42:04 -0800

    add full echo loops

--- zips.php
+++ zips.php
@@ -1,39 +1,25 @@
 <?php
 
-    // POPULATIONS
     $populations = file_get_contents("zipcodespopulation.json");
 
     $decoded_pops = json_decode($populations, true);
 
-    // ZIPS, CITIES, STATES
     $zips = file_get_contents("zipcodes.json");
 
     $decoded_zips = json_decode($zips, true);
 
-    // foreach ($decoded_zips as $key => $value) {
-    //     // echo $value['Zipcode'] . " " . $value['City'] . " " . $value['State'] . " \n";
-    //     $zipCode = $value['Zipcode'];
-    //     $city = $value['City'];
-    //     $state = $value['State'];
-    //     echo $zipCode . " " . $city . " " . $state . " \n";
-    // }
+    foreach ($decoded_zips as $key => $value) {
+        $zipCode = $value['Zipcode'];
+        $city = $value['City'];
+        $state = $value['State'];
 
-    foreach ($decoded_pops as $key => $value) {
-        echo $value['Zip Code ZCTA'] . " " . $value['2010 Census Population'] . " \n";
+        foreach ($decoded_pops as $key => $value) {
+          if ($value['Zip Code ZCTA'] == $zipCode) {
+            echo "population of " . $zipCode . " " . $city . " " . $state . " " .
+                $value['Zip Code ZCTA'] ." " . $value['2010 Census Population'] . "\n";
+            break;
+          }
+        }
     }
-
-
-
-    // foreach ($decoded_pops as $key => $value) {
-    //   if ($value['Zip Code ZCTA'] == $zipCode) {
-    //     $return_population = $value['2010 Census Population'];
-    //     return $return_population;
-    //   }
-    //   // return $return_population;
-    // }
-    // $population = $return_population;
-
-
-
 
   ?>
