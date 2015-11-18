eb18f1778f54a7f475e3300150f9f6adeece8985
Nathan <student@epicodus.com>
2015-11-17 16:53:25 -0800

    zips display properly

--- zips_display.php
+++ zips_display.php
@@ -1,8 +1,11 @@
 <?php
 
+// THIS WORKS NOW START HERE
+
 
     $zips = file_get_contents("zipcodes.json");
     $decoded_zips = json_decode($zips, true);
+    $counter = 0;
 
     foreach ($decoded_zips as $key => $value) {
         $zipCode = $value['Zipcode'];
@@ -13,9 +16,10 @@
         $population = $value['EstimatedPopulation'];
         echo $zipCode . " " . $city . " " . $state . " " . $population
         . "\n";
+        $counter += 1;
     }
 
-
+    echo $counter;
 
 
 
