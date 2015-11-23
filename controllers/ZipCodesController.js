86eee5a7c9fbbed9ba9a41b200287d784e24f876
Nathan Young <nathanyoung@Nathans-MacBook-Pro.local>
2015-11-22 18:37:33 -0800

    clean up code

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -4,7 +4,7 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
 
     $scope.zipCodesFB.orderByChild("digit3").equalTo(4).on("child_added", function(snapshot) {
         console.log(snapshot.key() + " is " + snapshot.val().digit3);
-        console.log(35928349850293845709238457);
+        console.log(2345);
     });
 
 
@@ -13,14 +13,11 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
     //
     //     $scope.allZipCodes = snapshot.val();
 
-
-
         // for (i = 0; i < $scope.allZipCodes.length; i++) {
         //   console.log(i);
         //   console.log($scope.allZipCodes[i].city);
         //   console.log($scope.allZipCodes[i].zipCode);
         // }
-
 
     // });
 
