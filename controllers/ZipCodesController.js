19695f41218d2a31127809cf67d977e306a3b1bb
Nathan <student@epicodus.com>
2015-11-24 13:53:16 -0800

    add detectChanges functions

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -15,6 +15,28 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope) {
         console.log(2345);
     });
 
+    // these functions detect input changes.
+
+    $scope.detectChangeDigit1 = function() {
+      console.log("Input 1 changed!");
+    };
+
+    $scope.detectChangeDigit2 = function() {
+      console.log("Input 2 changed!");
+    };
+
+    $scope.detectChangeDigit3 = function() {
+      console.log("Input 3 changed!");
+    };
+
+    $scope.detectChangeDigit4 = function() {
+      console.log("Input 4 changed!");
+    };
+
+    $scope.detectChangeDigit5 = function() {
+      console.log("Input 5 changed!");
+    };
+
 
 
     // $scope.zipCodesFB.on('value', function(snapshot) {
