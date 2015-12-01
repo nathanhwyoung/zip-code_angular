f5fce7d7a240647480601f380f6bf1dd70dc3bdb
Nathan <student@epicodus.com>
2015-11-30 16:58:47 -0800

    add compareArray

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -1,13 +1,14 @@
 zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope, $firebaseArray) {
 
+    // don't need this instantiation??!
     // databound to inputs
-    $scope.zipCode = [
-      {"firstDigit":""},
-      {"secondDigit":""},
-      {"thirdDigit":""},
-      {"fourthDigit":""},
-      {"fifthDigit":""}
-    ]
+    // $scope.zipCode = [
+    //   {"firstDigit":""},
+    //   {"secondDigit":""},
+    //   {"thirdDigit":""},
+    //   {"fourthDigit":""},
+    //   {"fifthDigit":""}
+    // ]
 
     // new firebase reference object
     var ref = new Firebase('https://zip-it.firebaseio.com/zips');
@@ -16,35 +17,85 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope, $firebaseArr
     $scope.detectChangeDigit1 = function() {
       var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
       $scope.matchedDigit1Array = $firebaseArray(query);
-      console.log($scope.matchedDigit1Array);
+      $scope.newGuess();
     };
 
     $scope.detectChangeDigit2 = function() {
-      var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
-      $scope.matchedDigit1Array = $firebaseArray(query);
-      console.log($scope.matchedDigit1Array);
+      var query = ref.orderByChild("digit2").equalTo($scope.zipCode.secondDigit.toString());
+      $scope.matchedDigit2Array = $firebaseArray(query);
+      $scope.newGuess();
     };
 
     $scope.detectChangeDigit3 = function() {
-      var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
-      $scope.matchedDigit1Array = $firebaseArray(query);
-      console.log($scope.matchedDigit1Array);
+      var query = ref.orderByChild("digit3").equalTo($scope.zipCode.thirdDigit.toString());
+      $scope.matchedDigit3Array = $firebaseArray(query);
+      $scope.newGuess();
     };
 
     $scope.detectChangeDigit4 = function() {
-      var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
-      $scope.matchedDigit1Array = $firebaseArray(query);
-      console.log($scope.matchedDigit1Array);
+      var query = ref.orderByChild("digit4").equalTo($scope.zipCode.fourthDigit.toString());
+      $scope.matchedDigit4Array = $firebaseArray(query);
+      $scope.newGuess();
     };
 
     $scope.detectChangeDigit5 = function() {
-      var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
-      $scope.matchedDigit1Array = $firebaseArray(query);
-      console.log($scope.matchedDigit1Array);
+      var query = ref.orderByChild("digit5").equalTo($scope.zipCode.fifthDigit.toString());
+      $scope.matchedDigit5Array = $firebaseArray(query);
+      $scope.newGuess();
     };
 
+    // GUESS FUNCTION
+    // should be refactored
     $scope.newGuess = function () {
+      var compareArray = [];
+      if(typeof $scope.zipCode.firstDigit !== "undefined") {
+        compareArray.push($scope.matchedDigit1Array);
+      }
+      if(typeof $scope.zipCode.secondDigit !== "undefined") {
+        compareArray.push($scope.matchedDigit2Array);
+      }
+      if(typeof $scope.zipCode.thirdDigit !== "undefined") {
+        compareArray.push($scope.matchedDigit3Array);
+      }
+      if(typeof $scope.zipCode.fourthDigit !== "undefined") {
+        compareArray.push($scope.matchedDigit4Array);
+      }
+      if(typeof $scope.zipCode.fifthDigit !== "undefined") {
+        compareArray.push($scope.matchedDigit5Array);
+      }
+      console.log(compareArray);
 
+      var resultsArray = [];
+
+      // should be refactored recursively
+      if (compareArray.length === 1) {
+        console.log(1);
+      } else if (compareArray.length === 2) {
+        console.log(2);
+      } else if (compareArray.length === 3) {
+        console.log(3);
+      } else if (compareArray.length === 4) {
+        console.log(4);
+      } else if (compareArray.length === 5) {
+        console.log(5);
+      }
+
+      // for(i=0; i < compareArray.length; i++) {
+      //   console.log(i);
+      // }
+
+      // angular.forEach($scope.zipCode, function(value, key) {
+      //   console.log(value);
+      //   if(typeof value !== "undefined") {
+      //     console.log("yes");
+      //   }
+      // });
+
+      // $scope.matchedDigit1Array.$loaded().then(function() {
+      //   angular.forEach($scope.matchedDigit1Array, function(value, key) {
+      //     console.log(value.city + value.population);
+        // });
+      // });
     }
 
     // guess function
@@ -64,6 +115,4 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope, $firebaseArr
     //                 " " + snapshot.val().state);
     //   });
     // }
-
-
 });
