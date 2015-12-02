cd144e0631eb9198a2c264cdf14c0aa77cadda87
Nathan <student@epicodus.com>
2015-12-01 16:16:50 -0800

    add id arrays

--- controllers/ZipCodesController.js
+++ controllers/ZipCodesController.js
@@ -13,34 +13,81 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope, $firebaseArr
     // new firebase reference object
     var ref = new Firebase('https://zip-it.firebaseio.com/zips');
 
-    // methods to detect changes from inputs
+    // methods to detect changes from inputs and query the DB
+    // should be refactored to only grab id's and NOT full objects
     $scope.detectChangeDigit1 = function() {
       var query = ref.orderByChild("digit1").equalTo($scope.zipCode.firstDigit.toString());
       $scope.matchedDigit1Array = $firebaseArray(query);
+      $scope.matchedDigit1ArrayIds = [];
+      $scope.matchedDigit1Array.$loaded().then(function() {
+        angular.forEach($scope.matchedDigit1Array, function(value, key) {
+          $scope.matchedDigit1ArrayIds.push(value.$id);
+        });
+        $scope.matchedDigit1ArrayIds.sort();
+        console.log($scope.matchedDigit1ArrayIds);
+        console.log($scope.matchedDigit1Array);
+      });
       $scope.newGuess();
     };
 
     $scope.detectChangeDigit2 = function() {
       var query = ref.orderByChild("digit2").equalTo($scope.zipCode.secondDigit.toString());
       $scope.matchedDigit2Array = $firebaseArray(query);
+      $scope.matchedDigit2ArrayIds = [];
+      $scope.matchedDigit2Array.$loaded().then(function() {
+        angular.forEach($scope.matchedDigit2Array, function(value, key) {
+          $scope.matchedDigit2ArrayIds.push(value.$id);
+        });
+        $scope.matchedDigit2ArrayIds.sort();
+        console.log($scope.matchedDigit2ArrayIds);
+        console.log($scope.matchedDigit2Array);
+      });
       $scope.newGuess();
     };
 
     $scope.detectChangeDigit3 = function() {
       var query = ref.orderByChild("digit3").equalTo($scope.zipCode.thirdDigit.toString());
       $scope.matchedDigit3Array = $firebaseArray(query);
+      $scope.matchedDigit3ArrayIds = [];
+      $scope.matchedDigit3Array.$loaded().then(function() {
+        angular.forEach($scope.matchedDigit3Array, function(value, key) {
+          $scope.matchedDigit3ArrayIds.push(value.$id);
+        });
+        $scope.matchedDigit3ArrayIds.sort();
+        console.log($scope.matchedDigit3ArrayIds);
+        console.log($scope.matchedDigit3Array);
+      });
       $scope.newGuess();
     };
 
     $scope.detectChangeDigit4 = function() {
       var query = ref.orderByChild("digit4").equalTo($scope.zipCode.fourthDigit.toString());
       $scope.matchedDigit4Array = $firebaseArray(query);
+      $scope.matchedDigit4ArrayIds = [];
+      $scope.matchedDigit4Array.$loaded().then(function() {
+        angular.forEach($scope.matchedDigit4Array, function(value, key) {
+          $scope.matchedDigit4ArrayIds.push(value.$id);
+
+        });
+        $scope.matchedDigit4ArrayIds.sort();
+        console.log($scope.matchedDigit4ArrayIds);
+        console.log($scope.matchedDigit4Array);
+      });
       $scope.newGuess();
     };
 
     $scope.detectChangeDigit5 = function() {
       var query = ref.orderByChild("digit5").equalTo($scope.zipCode.fifthDigit.toString());
       $scope.matchedDigit5Array = $firebaseArray(query);
+      $scope.matchedDigit5ArrayIds = [];
+      $scope.matchedDigit5Array.$loaded().then(function() {
+        angular.forEach($scope.matchedDigit5Array, function(value, key) {
+          $scope.matchedDigit5ArrayIds.push(value.$id);
+        });
+        $scope.matchedDigit5ArrayIds.sort();
+        console.log($scope.matchedDigit5ArrayIds);
+        console.log($scope.matchedDigit5Array);
+      });
       $scope.newGuess();
     };
 
@@ -49,41 +96,39 @@ zipCodeApp.controller('ZipCodesCtrl', function ZipCodesCtrl($scope, $firebaseArr
     $scope.newGuess = function () {
       var compareArray = [];
       if(typeof $scope.zipCode.firstDigit !== "undefined") {
-        compareArray.push($scope.matchedDigit1Array);
+        compareArray.push($scope.matchedDigit1ArrayIds);
       }
       if(typeof $scope.zipCode.secondDigit !== "undefined") {
-        compareArray.splice(1, 0, $scope.matchedDigit2Array);
-        // compareArray.push($scope.matchedDigit2Array);
+        compareArray.splice(1, 0, $scope.matchedDigit2ArrayIds);
       }
       if(typeof $scope.zipCode.thirdDigit !== "undefined") {
-        compareArray.splice(2, 0, $scope.matchedDigit3Array);
-        // compareArray.push($scope.matchedDigit3Array);
+        compareArray.splice(2, 0, $scope.matchedDigit3ArrayIds);
       }
       if(typeof $scope.zipCode.fourthDigit !== "undefined") {
-        compareArray.splice(3, 0, $scope.matchedDigit4Array);
-        // compareArray.push($scope.matchedDigit4Array);
+        compareArray.splice(3, 0, $scope.matchedDigit4ArrayIds);
       }
       if(typeof $scope.zipCode.fifthDigit !== "undefined") {
-        compareArray.splice(4, 0, $scope.matchedDigit5Array);
-        // compareArray.push($scope.matchedDigit5Array);
+        compareArray.splice(4, 0, $scope.matchedDigit5ArrayIds);
       }
 
       var resultsArray = [];
 
       // should be refactored recursively
+
+      // if there is only one number, only one query is needed, and it can be
+      // assigned to the results array.
       if (compareArray.length === 1) {
-        resultsArray = compareArray;
-        // console.log(compareArray);
-        angular.forEach(resultsArray, function(value, key) {
-          // console.log(value);
-        })
-      } else if (compareArray.length === 2) {
-        console.log(compareArray[1]);
-        // angular.forEach(compareArray[0], function(value, key) {
-        //   console.log(key);
-        // });
-        // console.log(2);
-      } else if (compareArray.length === 3) {
+
+      }
+
+      // if there are two numbers, we need two queries, and the resulting arrays
+      // need to be checked against each other to find zipcodes that are only
+      // present in both arrays.
+      else if (compareArray.length === 2) {
+
+      }
+
+      else if (compareArray.length === 3) {
         console.log(compareArray[2]);
       } else if (compareArray.length === 4) {
         console.log(compareArray[3]);
